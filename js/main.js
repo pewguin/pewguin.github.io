67017f627cf07dedd2c9346a03b03e51796d21fd
Jonas Brastad <jonas.brastad@misd400.org>
2024-10-06 20:24:02 -0700

    wowza

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ function load() {
     $("#header").load("/components/header.html"); 
 }
 
+function jscmd(i) {
+    if (i == 4) {
+        return "tcfwdcwmfttdzl"
+    }
+}
 
 window.addEventListener("load", load)
